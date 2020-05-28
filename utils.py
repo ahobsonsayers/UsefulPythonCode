a8b0328bbd8600e8e482aada9336cf0810f0c30d
Arran Hobson Sayers <32173585+ahobsonsayers@users.noreply.github.com>
2020-05-28 19:09:17 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,55 @@
-import subprocess
-import shutil
-import git
-from typing import List, Dict
-from pathlib import Path
-import sys
+
 import contextlib
 import os
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+from typing import Dict, List
+
+import git
+
+
+def execute(cmd: List[str], cwd: str = None, env: Dict = {}):
+    print(" ".join(cmd))
+
+    env_dict = {**os.environ, **env}
+    env_dict["SYSTEMD_COLORS"] = "1"
+
+    # universal_newlines makes sure outputs strings instead of byits
+    with subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        bufsize=-1,
+        universal_newlines=True,
+        cwd=cwd,
+        env=env_dict,
+    ) as process:
+        for line in process.stdout:
+            print(line, end="")  # Print line here. End print with empty string instead of newline
+
+    if process.returncode != 0:
+        print("Command '{0}' failed with exit code {1}".format(" ".join(process.args), process.returncode))
+        sys.exit(process.returncode)
+
+
+def check_requirements(programs: List[str]):
+
+    for program in programs:
+        if shutil.which(program) is None:
+            raise Exception("{0} not installed or accessable on the commandline. Exiting".format(program))
+
+
+@contextlib.contextmanager
+def working_directory(path):
+    """Changes working directory and returns to previous on exit."""
+    prev_cwd = Path.cwd()
+    os.chdir(str(path))
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
 
 
 # Get the go communication package
@@ -33,12 +77,35 @@ def get_repo(repo: str, directory: Path):
         git_repo.git.pull("origin", "master")
 
 
-@contextlib.contextmanager
-def working_directory(path):
-    """Changes working directory and returns to previous on exit."""
-    prev_cwd = Path.cwd()
-    os.chdir(str(path))
-    try:
-        yield
-    finally:
-        os.chdir(prev_cwd)
+# See https://bugs.python.org/issue26660
+# Took from https://github.com/python/cpython/commit/e9b51c0ad81da1da11ae65840ac8b50a8521373c
+# Required for windows as src folder in temp fil refuses to be deleted
+def rm_temp(name):
+    def onerror(func, path, exc_info):
+        if issubclass(exc_info[0], PermissionError):
+
+            def resetperms(path):
+                try:
+                    os.chflags(path, 0)
+                except AttributeError:
+                    pass
+                os.chmod(path, 0o700)
+
+            try:
+                if path != name:
+                    resetperms(os.path.dirname(path))
+                resetperms(path)
+
+                try:
+                    os.unlink(path)
+                # PermissionError is raised on FreeBSD for directories
+                except (IsADirectoryError, PermissionError):
+                    rm_temp(path)
+            except FileNotFoundError:
+                pass
+        elif issubclass(exc_info[0], FileNotFoundError):
+            pass
+        else:
+            raise
+
+    shutil.rmtree(name, onerror=onerror)
